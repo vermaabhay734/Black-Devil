23fd3787566b2995e6bfad5fa20412f3d7b897d0
Abhay <vermaabhay734@gmail.com>
2025-09-05 00:35:23 +0530

    Auto-generated commit

---
+++ server/angular-app-engine-manifest.mjs
@@ -0,0 +1,10 @@
+
+export default {
+  basePath: 'https://vermaabhay734.github.io/Black-Devil',
+  supportedLocales: {
+  "en-US": ""
+},
+  entryPoints: {
+    '': () => import('./main.server.mjs')
+  },
+};
